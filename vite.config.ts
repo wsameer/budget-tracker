a567a369b6d5f06d268650185b636cb4c239c0e1
wsameer <dev.wsameer@gmail.com>
2024-08-05 13:37:04 -0400

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [react(), viteTsconfigPaths()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  optimizeDeps: { exclude: ['fsevents'] },
 })
