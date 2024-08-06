c450ea14292fbc8a57b1b33fbe8e9cd7cb35c292
wsameer <dev.wsameer@gmail.com>
2024-08-06 16:31:29 -0400

    adds navigation to the app

---
+++ src/components/navigation/index.tsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import { BarChart2Icon, CreditCardIcon, FileTextIcon, HomeIcon, Settings, WalletMinimal } from 'lucide-react';
+import { NavLink, useNavigate } from "react-router-dom";
+
+import { useResponsive } from '@/hooks/useResponsive';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { cn } from '@/utils/cn';
+import { Button } from '../ui/button';
+
+type SideNavigationItem = {
+  icon: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
+  path: string;
+  label: string;
+};
+
+export const Navigation = () => {
+  const { isMobile } = useResponsive();
+  const navigate = useNavigate();
+
+  const navItems = [
+    { icon: HomeIcon, label: 'Dashboard', path: '/app' },
+    { icon: FileTextIcon, label: 'Transactions', path: '/app/transactions' },
+    { icon: CreditCardIcon, label: 'Accounts', path: '/app/accounts' },
+    { icon: BarChart2Icon, label: 'Stats', path: '/app/stats' },
+    { icon: Settings, label: 'Settings', path: '/app/settings' },
+  ].filter(Boolean) as SideNavigationItem[];
+
+  console.log();
+
+
+  const NavItem = ({ item }: { item: SideNavigationItem }) => {
+    const isActive = location.pathname === item.path;
+    return (
+      <Tooltip key={item.label}>
+        <TooltipTrigger asChild>
+          <NavLink
+            key={item.label}
+            to={item.path}
+            className={cn('flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8',
+              isActive && 'text-accent-foreground bg-accent'
+            )}
+          >
+            <item.icon className="h-5 w-5" />
+            <span className="sr-only">{item.label}</span>
+          </NavLink>
+        </TooltipTrigger>
+        <TooltipContent side="right">{item.label}</TooltipContent>
+      </Tooltip>
+    )
+  }
+
+  if (isMobile) {
+    return (
+      <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 flex justify-around items-center h-16">
+        {navItems.map((item, index) => (
+          <Button
+            key={index}
+            variant={location.pathname === item.path ? "default" : "ghost"}
+            size="icon"
+            onClick={() => navigate(item.path)}
+          >
+            <item.icon className="h-5 w-5" />
+          </Button>
+        ))}
+      </nav>
+    )
+  }
+
+  return (
+    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+      <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+        <NavLink
+          to="/app"
+          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+        >
+          <WalletMinimal className="h-4 w-4 transition-all group-hover:scale-110" />
+          <span className="sr-only">Budget Tracker</span>
+        </NavLink>
+
+        {navItems.slice(0, -1).map((item) => (
+          <NavItem key={item.label} item={item} />
+        ))}
+      </nav>
+      <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+        <NavItem item={navItems[navItems.length - 1]} />
+      </nav>
+    </aside >
+  )
+}
